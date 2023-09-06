3928a4bc5b0e5d198bbde7ef5cec593850cb40b0
Piotr <piotr.bania@europe.com>
2023-09-06 13:23:12 +0100

    init

--- app/gallery/Gallery.js
+++ app/gallery/Gallery.js
@@ -3,20 +3,38 @@ import '../gallery/gallery.scss'
 import React, { useRef } from 'react'
 import { useLoader, useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
-import { ShaderMaterial } from 'three'
+import { ShaderMaterial, TextureLoader } from 'three'
 
 import wave_vert  from '../../public/shaders/wave.vert'
 import wave_frag  from '../../public/shaders/wave.frag'
+import { Icosahedron } from '@react-three/drei'
+
+const images = [
+    '/images/1old.webp',
+    '/images/13.webp',
+    '/images/15sml.webp',
+    '/images/21.webp',
+    '/images/29sml.webp',
+    '/images/47.webp',
+    '/images/55sml.webp',
+    '/images/76.webp',
+    '/images/84old.webp',
+]
 
 const Gallery = () => {
-    const [image_1] = useLoader(THREE.TextureLoader, ['./images/55sml.webp'])
+    const getRandomImage = () => {
+        return images[Math.floor(Math.random() * images.length)]
+    }
+
+    const textureLoader = new TextureLoader()
+    const placeholderTexture = textureLoader.load(getRandomImage)
 
     const waveMaterial = useRef(new ShaderMaterial({
         uniforms: {
             uTime: { value: 0 },
             uFrequency: { value: new THREE.Vector2(0.5, 0.25) },
             uTransparency: { value: 1 },
-            uTexture: { value: image_1 },
+            uTexture: { value: placeholderTexture },
         },
         vertexShader: wave_vert,
         fragmentShader: wave_frag,
@@ -27,11 +45,38 @@ const Gallery = () => {
         waveMaterial.current.uniforms.uTime.value += delta
     })
 
+    const icosahedronVertices = []
+    const radius = 15
+
+    for (let i = 0; i < 42; i++) {
+        const phi = Math.acos(-1 + (2 * i) / 42)
+        const theta = Math.sqrt(42 * Math.PI) * phi
+        const x = radius * Math.cos(theta) * Math.sin(phi)
+        const y = radius * Math.sin(theta) * Math.sin(phi)
+        const z = radius * Math.cos(phi)
+        icosahedronVertices.push({ x, y, z })
+    }
+
     return (
-        <mesh position={[0, -1, 4]}>
-            <planeGeometry args={[30, 20, 150, 100]} />
-            <primitive object={waveMaterial.current} />
-        </mesh>
+        <Icosahedron args={[radius, 1, 1]}>
+            {icosahedronVertices.map((vertex, index) => (
+                <mesh
+                    key={index}
+                    position={[vertex.x, vertex.y, vertex.z]}
+                    rotation={[0, 0, 0]}
+                    onUpdate={(self) => {
+                        self.lookAt(new THREE.Vector3(0, 0, 0))
+                    }}
+                >
+                    <planeGeometry args={[6, 4, 15, 10]} />
+                    <primitive
+                        object={waveMaterial.current}
+                    />
+                    <meshBasicMaterial map={textureLoader.load(getRandomImage())} side={THREE.DoubleSide}/>
+                </mesh>
+            ))}
+            <meshBasicMaterial wireframe color={'#1e1e1e'} />
+        </Icosahedron>
     )
 }
 
--- app/gallery/page.js
+++ app/gallery/page.js
@@ -2,7 +2,7 @@
 
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { PerspectiveCamera } from '@react-three/drei'
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import { motion as m } from 'framer-motion'
 import Gallery from './Gallery'
 
@@ -14,10 +14,22 @@ const page = () => {
             animate={{opacity: 1, transition: { delay: 0.1, duration: 2}}}
             >
             <Canvas>
-                <PerspectiveCamera makeDefault position={[0, 0, 35]} />
+                <PerspectiveCamera makeDefault position={[0, 0, 23]} />
                 <Suspense fallback={null}>
                     <Gallery />
                 </Suspense>
+                <OrbitControls
+                    autoRotate={true}
+                    autoRotateSpeed={0.25 * -1}
+                    enablePan={false}
+                    enableZoom={false}
+                    // minPolarAngle={Math.PI / 2}
+                    // maxPolarAngle={Math.PI / 2}
+                    // minDistance={25}
+                    // maxDistance={25}
+                    enableDamping={true}
+                    dampingFactor={0.01}
+                />
             </Canvas>
         </m.div>
     )
